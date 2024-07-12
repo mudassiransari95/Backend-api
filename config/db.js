713e14bc5270f40dcac021b16b47efc9948702b3
mudassir <mudassir95546589@gmail.com>
2024-07-12 15:39:53 +0530

    change

--- config/db.js
+++ config/db.js
@@ -1,6 +1,7 @@
 const mongoose=require('mongoose')
+require('dotenv').config()
 const connectToDb=async()=>{
-    mongoose.connect('mongodb://0.0.0.0:27017/App')
+    mongoose.connect(process.env.MONGO-URL)
     .then(()=>console.log('connected to Mongodb successfully'))
     .catch(()=>console.log('error in connectin to mongodb'));
 }
